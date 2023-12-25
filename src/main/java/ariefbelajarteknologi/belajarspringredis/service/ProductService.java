cab05b45a5acbb2dd07772bebc956a6f27a4b01d
Arief-Karditya <nolifegansaya@gmail.com>
2023-12-25 14:20:35 +0700

    Spring Caching, add Service Class

---
+++ src/main/java/ariefbelajarteknologi/belajarspringredis/service/ProductService.java
@@ -0,0 +1,34 @@
+package ariefbelajarteknologi.belajarspringredis.service;
+
+import ariefbelajarteknologi.belajarspringredis.entity.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ProductService {
+
+    @Cacheable(value = "products", key = "#id")
+    public Product getProduct(String id) {
+        log.info("Get Product {}", id);
+        return Product.builder()
+                .id(id)
+                .name("example")
+                .price(1_000L)
+                .build();
+    }
+
+    @CachePut(value = "products", key = "#product.id")
+    public Product save(Product product) {
+        log.info("Save Product {}", product);
+        return product;
+    }
+
+    @CacheEvict(value = "products", key = "#id")
+    public void remove(String id) {
+        log.info("Remove product {}", id);
+    }
+}
